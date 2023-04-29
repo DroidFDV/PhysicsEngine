4469c2f505c1f6676e4ed0932617d9e18fc3f62b
Sirazetdinov Rustem <avesirazetdinov@gmail.com>
2023-04-29 17:28:29 +0300

    create utils.hpp file

---
+++ include/utils.hpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+
+#define _AXC_BEGIN namespace axc {
+#define _AXC_END   }
+#define _AXC       ::axc::
+#define _STD       ::std::
+
+
+_AXC_BEGIN
+
+inline void _AXC_VERIFY (bool expression, const char* msg) {
+    if (expression == true)
+    {
+        // std::cerr << '\n' << msg << '\n';
+        std::exit(134);
+    }
+}
+
+template <
+    class _Ty
+>
+inline void swap (_Ty& _Left, _Ty& _Right) noexcept {
+    if (addresof(_Left) != addresof(_Right))
+    {
+        _Ty _Tmp = move(_Left);
+        _Left    = move(_Right); 
+        _Right   = move(_Tmp);
+    } 
+}
+
+_AXC_END
