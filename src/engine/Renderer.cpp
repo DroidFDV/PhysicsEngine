4ceda9c7d56881a330f5debfb66bca0be82e7f3b
DroidFDV <Droid.World@yandex.ru>
2023-05-27 01:59:41 +0300

    create Renderer class

---
+++ src/engine/Renderer.cpp
@@ -0,0 +1,37 @@
+#include "Arbiter.cpp"
+
+
+class Renderer
+{
+public:
+        
+    explicit Renderer(sf::RenderTarget& target) : _Target(target) {}
+
+    void render(const Arbiter& Arbiter) 
+{
+            const std::vector<float> Constraint = Arbiter.getConstraint();
+            sf::CircleShape ConstraintBackground { Constraint[2] };
+            ConstraintBackground.setOrigin(Constraint[2], Constraint[2]);
+            ConstraintBackground.setFillColor(sf::Color::Black);
+            ConstraintBackground.setPosition(Constraint[0], Constraint[1]);
+            ConstraintBackground.setPointCount(128);
+            _Target.draw(ConstraintBackground);
+
+            sf::CircleShape ObjectCircle {1.0f};
+            ObjectCircle.setPointCount(32);
+            ObjectCircle.setOrigin(1.0f, 1.0f);
+            const auto& ObjectsVector = Arbiter.getObjects();
+
+            for (const auto& object : ObjectsVector) 
+            {
+                ObjectCircle.setPosition(object.posNow.Xcoord, object.posNow.Ycoord);
+                ObjectCircle.setScale(object.radius, object.radius);
+                ObjectCircle.setFillColor(object.color);
+                _Target.draw(ObjectCircle);
+            }
+        }
+
+private:
+
+        sf::RenderTarget& _Target;
+};
