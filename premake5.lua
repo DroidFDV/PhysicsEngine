83553b1b1946d0637a241fe7e00d65875f9b9e87
DroidFDV <Droid.World@yandex.ru>
2023-05-27 06:15:09 +0300

    update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,7 +8,10 @@ project "Physics Engine"
     location "build/PhysicsEngine"
     kind "ConsoleApp"
     language "C++"
+    cppdialect "C++17"
+
     -- files { "**.h", "**.hpp", "**.cpp" }
+
     files {
         "include/Utils.hpp",
         "include/Math.hpp",
@@ -19,8 +22,14 @@ project "Physics Engine"
         "src/engine/physics/bodies/**.cpp",
         "src/engine/**.hpp"
     }
+
+    includedirs {
+        "include"
+    }
+
     libdirs { os.findlib("SFML")}
     links { "sfml-system", "sfml-window", "sfml-graphics" }
+
     filter { "configurations:Debug" }
         defines { "DEBUG" }
         symbols "On"
@@ -28,3 +37,23 @@ project "Physics Engine"
     filter { "configurations:Release" }
         defines { "NDEBUG" }
         optimize "On"
+
+-- outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+newaction {
+    trigger = "clean",
+    description = "Remove all binaries and intermediate binaries, and vs files.",
+    execute = function()
+        print("Removing binaries")
+        os.rmdir("./build")
+        print("Removing intermediate binaries")
+        os.rmdir("./bin-int")
+        print("Removing project files")
+        os.rmdir("./.vs")
+        os.remove("**.sln")
+        os.remove("**.vcxproj")
+        os.remove("**.vcxproj.filters")
+        os.remove("**.vcxproj.user")
+        print("Done")
+    end
+}
