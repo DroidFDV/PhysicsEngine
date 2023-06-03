73338d891c403b117b7d8d9f7840f6d7af01f1bb
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-06-03 21:34:20 +0300

    finish RigidBody::set

--- src/engine/physics/rigidbody/RigidBody.cpp
+++ src/engine/physics/rigidbody/RigidBody.cpp
@@ -2,7 +2,7 @@
 #include "../../../../include/Utils.hpp"
 
 
-RigidBody::RigidBody() noexcept :
+RigidBody::RigidBody(body_traits::type bodyType) noexcept :
     Position(0.0f, 0.0f),
     Rotation(0.0f),
     Velocity(0.0f, 0.0f),
@@ -15,7 +15,10 @@ RigidBody::RigidBody() noexcept :
     // Friction(0.2f),
     Force(0.0f, 0.0f),
     Torque(0.0f)
-{}
+{ 
+    BodyTraits.bTy = bodyType;
+    // BodyTraits.bTy(bodyType)
+}
     
 
 void RigidBody::set (const Point& position, const Gvector& size, float mass) {
@@ -33,6 +36,15 @@ void RigidBody::set (const Point& position, const Gvector& size, float mass) {
     Force    = Gvector(0.0f, 0.f);
     Torque   = 0.0f;
 
+    if (mass < FLT_MAX) { // super massive rigid body
+        InvMass = 1.0f / mass;
+        InertiaTensor = mass * dot(Size, Size) / 12.0f; // TODO Why 12.0f?
+        InvI = 1.0f / InertiaTensor;
+    } else {
+        InvMass = 0.0f;
+        InertiaTensor = FLT_MAX;
+        InvI = 0.0f;
+    }
 }
 
 
