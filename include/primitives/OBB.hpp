a0a1cabb07f79232c536937da6b02a3eb4e2aa6b
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-06-03 21:36:36 +0300

    update class OBB

--- include/primitives/OBB.hpp
+++ include/primitives/OBB.hpp
@@ -1,65 +1,63 @@
-//
-
 #ifndef OBB_H
 #define OBB_H
 
-#include "../Math.hpp"
-#include "../bodies/RigidBody.h"
 
+#include "../Math.hpp"
+#include "IShell.h"
 
 
 using namespace _math;
 
-struct OBB {
+
+class OBB : public IShell {
     
-    // using position = _Mybody->position;
-    
+    // TODO i should calculate Size other way
     OBB (const Point& maxCorner, const Point& minCorner) noexcept :
-        size(maxCorner- minCorner),
-        _Mybody(nullptr)
+        IShell(nullptr),
+        Size(maxCorner - minCorner)
     {}
     
-    // TODO its incorrect
+    // TODO Size there incorrect
     Point getMaxCorner() const {
-        _AXC _VERIFY (_Mybody == nullptr, "_Mybody is nullptr!");
-        return Point(_Mybody->position + size * 0.5f);
+        _AXC _VERIFY (Mybody == nullptr, "Mybody is nullptr!");
+        return Point(Mybody->Position + Size * 0.5f);
     } 
     
-    // TODO int incorrect
+    // TODO Size there incorrect
     Point getMinCorner() const {
-        _AXC _VERIFY (_Mybody == nullptr, "_Mybody is nullptr!");
-        return Point(_Mybody->position - size * 0.5f);
+        _AXC _VERIFY (Mybody == nullptr, "Mybody is nullptr!");
+        return Point(Mybody->Position - Size * 0.5f);
     }
 
     //
-    std::vector<Point> getVertices() const {
-        _AXC _VERIFY (_Mybody == nullptr, "_Mybody is nullptr!");
+    std::vector<Point> getVertices() const 
+    {
+        _AXC _VERIFY (Mybody == nullptr, "Mybody is nullptr!");
         std::vector<Point> vertices; 
 
-        Point minCorner = _Mybody->position + size * 0.5f;
-        Point maxCorner = _Mybody->position - size * 0.5f;
+        Point minCorner = Mybody->Position + Size * 0.5f;
+        Point maxCorner = Mybody->Position - Size * 0.5f;
        
-        vertices.push_back(minCorner);
-        vertices.push_back(Point(minCorner.Xcoord, maxCorner.Ycoord));
         vertices.push_back(maxCorner);
+        vertices.push_back(Point(minCorner.Xcoord, maxCorner.Ycoord));
+        vertices.push_back(minCorner);
         vertices.push_back(Point(maxCorner.Xcoord, minCorner.Ycoord));
 
-        if ( !AlmostEqual(rotation, 0.0f) ) {
-            SQmatrix RotMatrix(rotation);
-
+        if ( !AlmostEqual(Rotation, 0.0f) )
+        {
+            SQmatrix RotMatrix(Rotation);
             for (Point& vertice : vertices) {
-                vertice = vertice.rotate(_Mybody->position, RotMatrix);
+                vertice = vertice.rotate(Mybody->Position, RotMatrix);
             }
         }
-
         return vertices;
     }
     
-
     
-    RigidBody* _Mybody;
-    Gvector    size;   // [width, height]
-    float      rotation;
+    
+    RigidBody* Mybody;
+    Gvector    Size;     // [width, height]
+    float      Rotation;
 };
 
 
