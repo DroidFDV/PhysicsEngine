0338d49edf33c02a136f6cef345db5a0793d9f8f
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-06-03 21:37:47 +0300

    add BodyTraits as a static member

--- include/rigidbody/RigidBody.h
+++ include/rigidbody/RigidBody.h
@@ -5,15 +5,30 @@
 #include "../Math.hpp"
 #include "../structures/Gvector.hpp"
 #include "../structures/Point.hpp"
+#include <type_traits>
 
 
 using namespace _math;
 using namespace _st;
 
 
+union body_traits {
+   
+    enum class type {
+        Circle,
+        // Triangle,
+        Box
+    } bTy;
+
+};
+
+
 struct RigidBody {
 
-    RigidBody() noexcept;
+    using traits = std::underlying_type<body_traits>;
+    
+    // TODO do i need constexpr here?
+    RigidBody(body_traits::type bodyType) noexcept;
 
     // void set (const Point& position, const Gvector& width, float mass) noexcept;
 
@@ -21,25 +36,30 @@ struct RigidBody {
 
     void applyForce (const Gvector& force) noexcept;
     
-   
+    void updatePosition (float dt); 
+
+    void updateVelocity (const Gvector& velocity, float dt);
+
+    void addVelocity (const Gvector& velocity, float dt);
     
-    ////////////////////////////////////////////////////////////
+    
+    
+    //////////////////////////////////////////////////////////// 
+    /// Rigid body properties
+        static body_traits BodyTraits;
+
     /// State
         Point   Position;
         float   Rotation;
         Gvector Velocity;
         float   AngularVelocity;
-    ////////////////////////////////////////////////////////////
     
-    ////////////////////////////////////////////////////////////
     /// Box properties
         Gvector Size;
         float   Mass, InvMass;
         float   InertiaTensor, InvI;
         // float   Friction;
-    ////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////
     /// Applied forces
         Gvector Force;
         float   Torque;
