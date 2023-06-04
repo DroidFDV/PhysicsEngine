ff1024a95e673973861c790853c77165f9b79027
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-06-04 21:14:07 +0300

    add getters

--- include/primitives/IShell.h
+++ include/primitives/IShell.h
@@ -12,6 +12,57 @@ public:
     
     virtual ~IShell() = 0;
     
+    // TODO _AXC _VERIFY(Mybody == nullprt, "Mybody is nullprt!!!")
+    const _st::Point& getPosition() const noexcept {
+        return Mybody->Position;
+    }
+
+    float getRotation() const noexcept {
+        return Mybody->Rotation;
+    }
+
+    const _st::Gvector& getVelocity() const noexcept {
+        return Mybody->Velocity;
+    } 
+
+    float getAngularVelocity() const noexcept {
+        return Mybody->AngularVelocity;
+    }
+
+    const _st::Gvector& getSize() const noexcept {
+        return Mybody->Size;
+    }  
+    
+    float getMass() const noexcept {
+        return Mybody->Mass;
+    }
+    
+    float getInvMass() const noexcept {
+        return Mybody->InvMass;
+    }
+
+    float getInertiaTensor() const noexcept {
+        return Mybody->InertiaTensor;
+    }
+
+    float getInvI() const noexcept {
+        return Mybody->InvI;
+    }
+    
+    float getFriction() const noexcept {
+        return Mybody->Friction;
+    }
+
+    const _st::Gvector& getForce() const noexcept {
+        return Mybody->Force;
+    }
+
+    float getTorque() const noexcept {
+        return Mybody->Torque;
+    }
+
+
+
 public:
     RigidBody* Mybody;
 };
