22a415aa338519ae08d66b1783dbfe61954ddf47
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-06-04 21:13:35 +0300

    change traits, add getRadius

--- include/primitives/Circle.hpp
+++ include/primitives/Circle.hpp
@@ -14,18 +14,23 @@ using namespace _math;
 
 class Circle : public IShell {
 public:
+    
+    using body_shape_type = RigidBody::shape_traits::type;
 
-    Circle (RigidBody* body, float radius = 1.0f) noexcept :
-        IShell(body),
-        Radius(radius)
-    {}
+
+    Circle (RigidBody* body) noexcept :
+        IShell(body)
+    {
+        if (body->ShapeTraits.shapeTy != body_shape_type::Circle) {
+            body->ShapeTraits.shapeTy = body_shape_type::Circle;
+        }
+    }
+
+    float getRadius() noexcept {
+        return this->getSize().Xcoord;
+    }
 
     ~Circle() override = default;
-
-
-
-public:
-    float Radius;
 };
 
 
