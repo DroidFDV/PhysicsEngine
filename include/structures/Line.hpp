9ae39029ae9298d630486be58dae5a9f48e442f5
listova-nastya <90141208+listova-nastya@users.noreply.github.com>
2023-06-05 02:40:53 +0300

    create docs

--- include/structures/Line.hpp
+++ include/structures/Line.hpp
@@ -1,37 +1,72 @@
 #ifndef LINE_H
 #define LINE_H
 
+
 #include "Point.hpp"
+
 
 _ST_BEGIN
 using namespace _math;
 
+
 ////////////////////////////////////////////////////////////
-/// \brief Utility struct for manipulating
-///        2-dimensional vectors. Type of values is float.
+/// \brief Utility struct for present a line 
 ///
 ////////////////////////////////////////////////////////////
 struct Line {
 
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a line through two given points
+    /// 
+    /// \param a first point
+    /// \param b second point
+    ///
+    ////////////////////////////////////////////////////////////
     Line (const Point& a, Point& b) noexcept : 
         begin(a),
         end(b),
         direction(end.Xcoord - begin.Xcoord, end.Ycoord - end.Ycoord)
     {}
 
-    // TODO: check correctness of the DirVector (!= 0) within template
-    // mb create a struct null_vector??
-    // Line (const Point& a, const Gvector& DirVector) :
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a line through point and direction
+    ///        vector
+    ///
+    /// \param a         point
+    /// \param dirVector direction vector
+    ///
+    /// TODO: check correctness of the dirVector (!= 0) within 
+    ///       template, mb create a struct null_vector??
+    ////////////////////////////////////////////////////////////
+    // Line (const Point& a, const Gvector& dirVector) :
     //     begin(a),
     // {}
 
-    // TODO check DirVector != 0
-    Line (const Gvector& DirVector) : 
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a line through point and direction
+    ///        vector
+    /// 
+    /// \param dirVector direction vector
+    ///
+    /// TODO: TODO check dirVector != 0
+    ////////////////////////////////////////////////////////////
+    Line (const Gvector& dirVector) : 
         begin(0,0),
         end(DirVector.Xcoord, DirVector.Xcoord),
         direction(DirVector) 
     {}
-
+    
+    ////////////////////////////////////////////////////////////
+    /// \brief rotate line by given angle with respect of origin
+    ///        point
+    /// 
+    /// \param Origin point to rotate with respect of it
+    /// \param angle  angle to rotate in degrees
+    ///
+    /// \return rotated line
+    ////////////////////////////////////////////////////////////
     Line rotate (const Point& Origin, float angle) noexcept {
         SQmatrix RotMatrix(angle);
         Point _Begin ( begin.rotate(Origin, RotMatrix) );
@@ -39,12 +74,19 @@ struct Line {
 
         return Line (_Begin, _End);
     }
-
+    
+    
+    
+    ////////////////////////////////////////////////////////////
+    /// Member data
+    ////////////////////////////////////////////////////////////
     Point   begin;
     Point   end;
     Gvector direction; 
 };
 
-_ST_END // END namespace st
 
+_ST_END // END namespace _math
+
+    
 #endif // POINT_H
