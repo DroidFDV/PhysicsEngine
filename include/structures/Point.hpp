5d38c50f207952326d11cd00d9a60445c17f8ecb
listova-nastya <90141208+listova-nastya@users.noreply.github.com>
2023-06-05 04:57:45 +0300

    Update Point.hpp

--- include/structures/Point.hpp
+++ include/structures/Point.hpp
@@ -7,33 +7,63 @@ _ST_BEGIN
 using namespace _math;
 
 ////////////////////////////////////////////////////////////
-/// \brief Utility struct for manipulating
-///        2-dimensional vectors. Type of values is float.
+/// Utility struct to present a point in R^2 
 ///
 ////////////////////////////////////////////////////////////
 struct Point : Gvector {
-
+    
+    ////////////////////////////////////////////////////////////
+    /// Construct a point through two given coords
+    /// 
+    /// \param x first coord
+    /// \param y second coord
+    ////////////////////////////////////////////////////////////
     Point (float x = 0.0f, float y = 0.0f) noexcept : Gvector(x,y) {}
 
+    ////////////////////////////////////////////////////////////
+    /// Construct a point through given vector
+    /// 
+    /// \param gvector vector to construct (a vector)
+    ////////////////////////////////////////////////////////////
     Point (const Gvector& gvector) noexcept : Gvector(gvector) {}
 
+    ////////////////////////////////////////////////////////////
+    /// Set point's coords
+    /// 
+    /// \param x first new coord
+    /// \param y second new coord
+    /// \return New point with (x,y) coords
+    ////////////////////////////////////////////////////////////
     Point& set (float x, float y) noexcept {
         Xcoord = x;
         Ycoord = y;
         return *this;
     }
 
-    //
+    ////////////////////////////////////////////////////////////
+    /// Rotate point with respect other point
+    /// 
+    /// \param Origin point to be rotated around it
+    /// \param AngleDegrees rotation angle in degrees
+    /// \return New rotated point
+    ////////////////////////////////////////////////////////////
     Point rotate (const Point& Origin, float AngleDegrees) const noexcept {
         SQmatrix RotMatrix(AngleDegrees);
         return Point( (RotMatrix * (*this - Origin)) + Origin );
     }
 
-    //
+    ////////////////////////////////////////////////////////////
+    /// Rotate point with respect other point
+    /// 
+    /// \param Origin point to be rotated around it
+    /// \param RotMatrix rotation matrix
+    /// \return New rotated point
+    ////////////////////////////////////////////////////////////
     Point rotate (const Point& Origin, const SQmatrix& RotMatrix) const noexcept {
         return Point( (RotMatrix * (*this - Origin)) + Origin );
     }
 };
+
 
 _ST_END // END namespace st
 
